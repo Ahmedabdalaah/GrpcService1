785a4dc047afc0024bfa95f876a44148c34d636c
Ahmed Abdallah <ahmedismail14141414@gmail.com>
2024-02-11 23:24:45 +0200

    Add project files.

---
+++ GRpcClient1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GRpcClient1;
+
+// The port number must match the port of the gRPC server.
+using var channel = GrpcChannel.ForAddress("http://localhost:5032");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(new HelloRequest { Name = "Ahmed"  , Address="Tanta"} );
+Console.WriteLine("Greeting: " + reply.Message);
+Console.ReadKey();
